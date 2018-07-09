56bb64ea20f9e99d8dac6ae6e2eb6309f5e541ea
EC2 Default User <ec2-user@ip-172-31-46-17.ap-southeast-1.compute.internal>
2018-07-09 13:50:36 +0000

    ログイン機能にかかる修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
   root 'hangouts#index'
   resources :users, only: [:show, :edit, :update]
   resources :hangouts
